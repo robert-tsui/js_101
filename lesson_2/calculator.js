85d782c669bba7930a4d59edcb4d70045f243f5a
Robert Tsui <rt.roberttsui@gmail.com>
2022-10-31 19:38:37 -0700

    initialized repo for JS 101

---
+++ lesson_2/calculator.js
@@ -0,0 +1,76 @@
+// ask the user for the first number
+// ask the user for the second number
+// ask the user for the operation
+// perform the operation
+// display the result of the operation 
+
+const readline = require('readline-sync');
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+prompt('Welcome to the Calculator!');
+
+function onceAgain() {
+  prompt('Great, let\'s play again!');
+}
+
+prompt('What is the first number?');
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt('Hmmm... that doesn\'t look like a valid number.');
+  number1 = readline.question();
+}
+
+prompt('What is the second number?');
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt('Hmmm... that doesn\'t look like a valid number.');
+  number2 = readline.question();
+}
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose either 1, 2, 3, or 4.');
+  operation = readline.question();
+}
+
+let output;
+
+switch (operation) {
+  case '1':
+    output = +number1 + +number2;
+    break;
+  case '2':
+    output = +number1 - +number2;
+    break;
+  case '3':
+    output = +number1 * +number2;
+    break;
+  case '4':
+    output = +number1 / +number2;
+    break;
+}
+prompt(`The result is ${output}.`);
+
+prompt('Would you like to play again?\n1) Yes 2) No');
+let playAgain = readline.question();
+
+while (!['1', '2'].includes(playAgain)) {
+  prompt('Must choose either 1 or 2.');
+  playAgain = readline.question()
+}
+
+if (playAgain === '1') {
+  onceAgain(); 
+} else {
+  prompt('Thank you for playing!');
+};
---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,98 @@
+// START 
+
+// a function that returns the sum of two numbers
+
+// SET results = value of summing two numbers
+
+// PRINT results
+
+// END
+
+-------------------------------
+
+// START
+
+// a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+// SET results = value of concatenating strings together
+
+// WHILE iterator <= length of array
+  // SET iterator = 0
+  // add the string at each index of the array to results
+  // iterator++
+
+// PRINT results
+
+// END
+
+-------------------------------
+
+// START
+
+// a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element. 
+
+// SET results = a new array with every other element of the original array
+// SET currentElement = current element of input array
+
+// WHILE iterator <= length of input array
+  // SET iterator = 1
+  // IF currentElement is not divisible by 2
+    // push currentElement to results 
+
+// PRINT results 
+
+
+-------------------------------
+
+// START
+
+// INPUT: string
+// OUTPUT: string
+
+/*
+A function that determines the index of the 3rd occurrence of a given character in a string. 
+
+For instance, if the given character is 'x' and the string is 'axbxcdxex',
+
+the function should return 6 (the index of the 3rd 'x'). 
+
+If the given character does not occur at least 3 times, return null.
+*/
+
+// SET result = index of the 3rd 'x'
+
+// WHILE iterator <= 3
+  // SET iterator = 0
+  // SET counter = 0
+  // IF current index is equal to char
+    // increment counter
+
+// PRINT result at the 3rd occurence of char
+
+-------------------------------
+
+// START
+
+// INPUT: 2 arrays
+// OUTPUT: 1 array
+
+/*
+a function that takes two arrays of numbers and returns the result of merging the arrays. 
+The elements of the first array should become the elements at the even indexes of the returned array, 
+while the elements of the second array should become the elements at the odd indexes.
+*/
+
+// SET result = new array that represents the merging of the two input arrays
+
+
+// WHILE iterator <= length of second input array
+  // SET iterator = 0
+  // push to result array
+  // WHILE iterator <= length of first input array
+    // SET iterator = 0
+    // push to result array
+
+// PRINT result
+
+
+
