0466d3111f5e0f47273ba454a0adc84d7a22083f
Robert Tsui <rt.roberttsui@gmail.com>
2022-11-08 15:31:19 -0800

    cleaned up i18n feature

--- lesson_02/calculator.js
+++ lesson_02/calculator.js
@@ -2,9 +2,8 @@
 // ask the user for the second number
 // ask the user for the operation
 // perform the operation
-// display the result of the operation 
+// display the result of the operation
 
-const LANGUAGE = 'en';
 const readline = require('readline-sync');
 const MESSAGES = require('./calculator_messages.json');
 
@@ -12,7 +11,7 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-function messages(message, lang='en') {
+function messages(message, lang = 'en') {
   return MESSAGES[lang][message];
 }
 
@@ -30,30 +29,23 @@ function onceAgain() {
 function calculator() {
   prompt(messages('firstNum','fr'));
   let number1 = readline.question();
-
   while (invalidNumber(number1)) {
     prompt(messages('incorrect','fr'));
     number1 = readline.question();
   }
-
   prompt(messages('secondNum','fr'));
   let number2 = readline.question();
-
   while (invalidNumber(number2)) {
     prompt(messages('incorrect','fr'));
     number2 = readline.question();
   }
-
   prompt(messages('operationAsk','fr'));
   let operation = readline.question();
-
   while (!['1', '2', '3', '4'].includes(operation)) {
     prompt(messages('mustChoose1','fr'));
     operation = readline.question();
   }
-
   let output;
-
   switch (operation) {
     case '1':
       output = +number1 + +number2;
@@ -69,18 +61,16 @@ function calculator() {
       break;
   }
   prompt(`The result is ${output}.`);
-
   prompt(messages('askToPlay','fr'));
   let playAgain = readline.question();
-
   while (!['1', '2'].includes(playAgain)) {
     prompt(messages('mustChoose2','fr'));
     playAgain = readline.question()
   }
-
   if (playAgain === '1') {
-    onceAgain(); 
+    onceAgain();
   } else {
     prompt(messages('thankYou','fr'));
   }
 }
+
