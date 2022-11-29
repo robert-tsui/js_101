c09967608832846959f8415b171fac837434db5e
Robert Tsui <rt.roberttsui@gmail.com>
2022-11-29 14:29:16 -0800

    1.6 solution #2

--- small_problems/easy_1.6.js
+++ small_problems/easy_1.6.js
@@ -39,6 +39,8 @@ sum: 1 + 2 + 3
 product: 1 * 2 * 3 = 6
 
 DATA STRUCTURE/ALGORITHM
+
+Solution #1
   
 1. require readlinesync module
 2. ask user to input an integer greater than 0
@@ -47,7 +49,20 @@ DATA STRUCTURE/ALGORITHM
 5. compute the sum or product of all numbers between 1 and entered integer, inclusive
 6. log chosen procedural output to the user
 
+Solution #2
+
+1. write a helper function to compute the sum of all numbers from 1 to entered integer
+2. write a helper function to compute the product of all numbers from 1 to entered integer
+3. require the readlinesync module
+4. ask the user to input an integer
+5. ask the user to select a mode of computation
+6. invoke the appropriate helper function
+7. log the message with either the sum or product of all numbers from 1 to entered integer
+
 */
+
+/* Solution #1
+
 
 const askUser = require(`readline-sync`);
 
@@ -79,4 +94,46 @@ if (inputOperand === `s`) {
   console.log(
     `The sum of the integers between 1 and ${inputInteger} is ${sum}.`
   );
+}
+
+*/
+
+// Solution #2
+
+function calculateSum(inputNum) {
+  let output = 0;
+  for (let i = 1; i <= inputNum; i++) {
+    output += i;
+  }
+
+  return output;
+}
+
+function calculateProduct(inputNum) {
+  let output = 1;
+  for (let i = 1; i <= inputNum; i++) {
+    output *= i;
+  }
+
+  return output;
+}
+
+let interact = require(`readline-sync`);
+let getNum = parseInt(
+  interact.question(`Please enter an integer greater than 0:\n`)
+);
+let getMode = interact.question(
+  `Enter "s" to compute the sum, or "p" to compute the product.\n`
+);
+
+if (getMode === `s`) {
+  let sum = calculateSum(getNum);
+  console.log(`The sum of the integers between 1 and ${getNum} is ${sum}.`);
+} else if (getMode === `p`) {
+  let product = calculateProduct(getNum);
+  console.log(
+    `The product of the integers between 1 and ${getNum} is ${product}.`
+  );
+} else {
+  console.log(`Oops, you must select an operation.`);
 }
