aa9ee1789469252987abef13ec61ac1a28171349
Robert Tsui <rt.roberttsui@gmail.com>
2022-12-01 12:40:03 -0800

    1.8fe

---
+++ small_problems/easy_1.8fe.js
@@ -0,0 +1,33 @@
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else {
+    return year % 400 === 0;
+  }
+}
+
+// function isLeapYear(year) {
+//   if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 400 === 0) {
+//     return true;
+//   } else {
+//     return year % 4 === 0;
+//   }
+// }
+
+console.log(isLeapYear(2016));
+console.log(isLeapYear(2015));
+console.log(isLeapYear(2100));
+console.log(isLeapYear(2400));
+console.log(isLeapYear(240000));
+console.log(isLeapYear(240001));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(1900));
+console.log(isLeapYear(1752));
+console.log(isLeapYear(1700));
+console.log(isLeapYear(1));
+console.log(isLeapYear(100));
+console.log(isLeapYear(400));
