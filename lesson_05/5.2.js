2673bc968310d1b4e126177696a593e864cb5b23
Robert Tsui <rt.roberttsui@gmail.com>
2023-01-10 09:12:50 -0800

    5.2

---
+++ lesson_05/5.2.js
@@ -0,0 +1,13 @@
+let words = ['go', 'ahead', 'and', 'jump'];
+
+words.sort((a, b) => {
+  if (a.length < b.length) {
+    return -1;
+  } else if (a.length > b.length) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
+console.log(words);
