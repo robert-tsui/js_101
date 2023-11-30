7587801e712325e8440efe650bf9114e6a2893f0
Robert Tsui <rt.roberttsui@gmail.com>
2023-11-30 07:44:05 -0800

    new attempt

--- small_problems/advanced_1.3.js
+++ small_problems/advanced_1.3.js
@@ -1,27 +1,27 @@
-const matrix = [
-  [1, 5, 8, 5],
-  [4, 7, 2, 0],
-  [3, 9, 6, 1],
-];
+// const matrix = [
+//   [1, 5, 8, 5],
+//   [4, 7, 2, 0],
+//   [3, 9, 6, 1],
+// ];
 
-function transpose(matrix) {
-  let newRowsCount = matrix[0].length;
-  let transposed = [];
+// function transpose(matrix) {
+//   let newRowsCount = matrix[0].length;
+//   let transposed = [];
 
-  for (let idx = 0; idx < newRowsCount; idx += 1) {
-    transposed.push([]);
-  }
+//   for (let idx = 0; idx < newRowsCount; idx += 1) {
+//     transposed.push([]);
+//   }
 
-  for (let rowIdx = 0; rowIdx < matrix.length; rowIdx += 1) {
-    for (let colIdx = 0; colIdx < matrix[rowIdx].length; colIdx += 1) {
-      transposed[colIdx].push(matrix[rowIdx][colIdx]);
-    }
-  }
+//   for (let rowIdx = 0; rowIdx < matrix.length; rowIdx += 1) {
+//     for (let colIdx = 0; colIdx < matrix[rowIdx].length; colIdx += 1) {
+//       transposed[colIdx].push(matrix[rowIdx][colIdx]);
+//     }
+//   }
 
-  return transposed;
-}
+//   return transposed;
+// }
 
-let newMatrix = transpose(matrix);
+// let newMatrix = transpose(matrix);
 
 // console.log(newMatrix); // [[1, 4, 3], [5, 7, 9], [8, 2, 6], [5, 0, 1]]
 // console.log(matrix); // [[1, 5, 8, 5], [4, 7, 2, 0], [3, 9, 6, 1]]
@@ -79,3 +79,62 @@ let newMatrix = transpose(matrix);
 //   transposed[1].push(matrix[2][1])
 
 //   transposed --> [[1, 4, 2], [2, 3, 6]]
+
+//------------------------------ new attempt
+function createNewArrayStructureForTranspose(originalMatrix) {
+  let transposedStructure = [];
+  let numberOfColumnsInOriginal = originalMatrix[0].length;
+  for (let column = 0; column < numberOfColumnsInOriginal; column++) {
+    transposedStructure.push([]);
+  }
+  return transposedStructure;
+}
+
+function transpose(matrix) {
+  let transposedMatrix = createNewArrayStructureForTranspose(matrix);
+
+  for (let originalCol = 0; originalCol < matrix.length; originalCol++) {
+    let currentColumn = matrix[originalCol];
+    for (
+      let originalRow = 0;
+      originalRow < matrix[originalCol].length;
+      originalRow++
+    ) {
+      let element = currentColumn[originalRow];
+      transposedMatrix[originalRow].push(element);
+    }
+  }
+  return transposedMatrix;
+}
+
+const matrix = [
+  [1, 5, 8, 5],
+  [4, 7, 2, 0],
+  [3, 9, 6, 1],
+];
+/* transposedMatrix
+[
+  [1, 4, 3],
+  [5, 7, 9],
+  [8, 2, 6],
+  [5, 0, 1],
+];
+*/
+
+let newMatrix = transpose(matrix);
+
+console.log(newMatrix); // [[1, 4, 3], [5, 7, 9], [8, 2, 6], [5, 0, 1]]
+// console.log(matrix); // [[1, 5, 8, 5], [4, 7, 2, 0], [3, 9, 6, 1]]
+
+console.log(transpose([[1, 2, 3, 4]])); // [[1], [2], [3], [4]]
+console.log(transpose([[1], [2], [3], [4]])); // [[1, 2, 3, 4]]
+console.log(transpose([[1]])); // [[1]]
+
+console.log(
+  transpose([
+    [1, 2, 3, 4, 5],
+    [4, 3, 2, 1, 0],
+    [3, 7, 8, 6, 2],
+  ])
+);
+// [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
